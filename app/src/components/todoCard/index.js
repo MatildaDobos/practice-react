f3b37d46dce2896e4981e04cd4a1a309e5f4bb35
Matilda Dobos <matilda.dobos@ness.com>
2019-12-17 16:27:12 +0200

    Refactor - add edit button

--- app/src/components/todoCard/index.js
+++ app/src/components/todoCard/index.js
@@ -20,24 +20,27 @@ const ToDoCard = props => {
     return (
         <div className='todo-info'>
             <div className='todo-info__header'>
-                <Link to={`/todo/${props.id}`}>
+                
                     <h1 className='todo-info__header__title'>{ props.title }</h1>
                     <h3 className='todo-info__header__flag'>Priority is high</h3>
-                </Link>
+                
                 <img onClick={ props.remove } src={ removeIcon } alt='Remove icon' className='todo-info__header__remove'/>
             </div>
             <div className='todo-info__body'>
                 <p className='todo-info__body__description'>{ props.description }</p>
             </div>
             <div className='todo-info__footer'>
-                <div className='todo-info__name'>Status: </div>
-                <div className='todo-info__value'>
-                    <Select 
-                        value={ status } 
-                        onChange={ (selectedOption) => props.onStatusChange(props.id, selectedOption.value) } 
-                        options={ todoStatusOptions } 
-                    />
+                <div className='todo-info__footer__status'>
+                    <div className='todo-info__name'>Status: </div>
+                    <div className='todo-info__value'>
+                        <Select 
+                            value={ status } 
+                            onChange={ (selectedOption) => props.onStatusChange(props.id, selectedOption.value) } 
+                            options={ todoStatusOptions } 
+                        />
+                    </div>
                 </div>
+                <Link to={`/todo/${props.id}`}><div className='todo-info__footer__link'>Edit</div></Link>
             </div>
         </div>
     );
