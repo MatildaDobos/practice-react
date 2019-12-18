fca137fbc65ae17d6b9f93b46baa76a578e470af
Matilda Dobos <matilda.dobos@ness.com>
2019-12-18 09:58:00 +0200

    Remove unnecessary subtitle

--- app/src/components/todoCard/index.js
+++ app/src/components/todoCard/index.js
@@ -20,10 +20,7 @@ const ToDoCard = props => {
     return (
         <div className='todo-info'>
             <div className='todo-info__header'>
-                
-                    <h1 className='todo-info__header__title'>{ props.title }</h1>
-                    <h3 className='todo-info__header__flag'>Priority is high</h3>
-                
+                <h1 className='todo-info__header__title'>{ props.title }</h1>
                 <img onClick={ props.remove } src={ removeIcon } alt='Remove icon' className='todo-info__header__remove'/>
             </div>
             <div className='todo-info__body'>
